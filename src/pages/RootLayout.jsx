a0ab56efba60f9fcfa4af7bd7f1cf4a4ee89e3f4
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-26 12:16:58 +0545

    Done 12

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -7,7 +7,7 @@ import SideBar from '../Components/SideBar';
 export default function RootLayout() {
     return (
         <Grid templateColumns={"repeat(6,1fr)"} bg="gray.50">
-            <GridItem as={"aside"} colSpan={{base:6,lg:2,xl:1}} bgColor={"purple.400"} minHeight={{lg:"100vh"}} p={{base:"30px",lg:"30px"}}>
+            <GridItem as={"aside"} colSpan={{base:6,lg:2,xl:1}} bgColor={"brand.600"} minHeight={{lg:"100vh"}} p={{base:"30px",lg:"30px"}}>
                 <SideBar/>
             </GridItem>
             <GridItem as="main" colSpan={{base:6,lg:4,xl:5}} p="40px">
