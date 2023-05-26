245386a4e666eb9d2617385cfad1e7e63e63e9f4
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-26 09:57:38 +0545

    Done 8

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,66 @@
+import {
+  ChatIcon,
+  CheckCircleIcon,
+  EmailIcon,
+  StarIcon,
+  WarningIcon
+} from '@chakra-ui/icons'
+import { TabList, TabPanels, Tabs, Tab, TabPanel, List, ListItem, ListIcon } from '@chakra-ui/react'
 import React from 'react'
 
 export default function Profile() {
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <Tabs mt="40px" p="20px" colorScheme={'purple'} variant={"enclosed"}>
+      <TabList>
+        <Tab _selected={{ color: "white", bg: "purple.400" }}>Account Info</Tab>
+        <Tab _selected={{ color: "white", bg: "purple.400" }}>Task History</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={EmailIcon} />
+              Email: prajwolneupane68@gmail.com
+            </ListItem>
+            <ListItem>
+              <ListIcon as={ChatIcon} />
+              Hi, I am Prajwol Neupane. I have been coding since 2020. Throughout my coding journey, I learn Java, Javascript, and different frameworks.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={StarIcon} />
+              In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
+            </ListItem>
+          </List>
+        </TabPanel>
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color="red.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color="red.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+          </List>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   )
 }
