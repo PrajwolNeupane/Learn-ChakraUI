71c30c19e7a03d30177dfae187c393af0442f9e4
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-26 11:55:40 +0545

    Done 9

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,9 +1,40 @@
+import { Box, Button, Checkbox, FormControl, FormHelperText, FormLabel, Input, Textarea } from '@chakra-ui/react'
 import React from 'react'
+import { Form,redirect } from 'react-router-dom'
 
 export default function Create() {
   return (
-    <div>
-      <h1>Create</h1>
-    </div>
+    <Box maxW={"480px"}>
+      <Form method='post' action='/create'>
+        <FormControl isRequired mb={"40px"} >
+          <FormLabel>Task name :</FormLabel>
+          <Input type='text' name="title" />
+          <FormHelperText>Enter a description task name.</FormHelperText>
+        </FormControl>
+        <FormControl isRequired mb={"40px"}>
+          <FormLabel>Task description :</FormLabel>
+          <Textarea placeholder='Enter a detailed description for the task...' name="description" />
+          <FormHelperText>Enter a description task name.</FormHelperText>
+        </FormControl>
+        <FormControl display={"flex"} alignItems={"center"} mb="40px" >
+          <Checkbox name="isPriority" size="lg" colorScheme={"purple"} />
+          <FormLabel>Make this a priority task.</FormLabel>
+        </FormControl>
+        <Button type='submit'>Submit</Button>
+      </Form>
+    </Box>
   )
+}
+export const createAction = async ({ request }) => {
+  const data = await request.formData()
+
+  const task = {
+    title: data.get('title'),
+    description: data.get('description'),
+    isPriority: data.get('isPriority') === ''
+  }
+
+  console.log(task)
+
+  return redirect('/')
 }
